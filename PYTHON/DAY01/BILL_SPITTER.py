4e70b9b36c8df367262a6bbd6c7d8907c4c147a9
aditya-saluja <adityasaluja2007@gmail.com>
2026-07-14 02:21:46 +0530

    Day 1: Started Internship Journey

---
+++ PYTHON/DAY01/BILL_SPITTER.py
@@ -0,0 +1,43 @@
+# chai = "Masala tea"
+# slice_char= chai[0:6]
+# print(slice_char
+
+BMI CALCULATOR
+
+
+weight = float(input("what is your weight: "))
+height = float(input("what is your height: "))
+
+Bmi = weight / (height * 2)
+
+print(f"{weight}kg")
+print(f"{height}mtr")
+
+print(Bmi)
+
+REPORT CARD 
+
+Name = input("what is your name: ")
+Roll_no = int(input("what is your roll no: "))
+Marks = int(input("marks in all 5 subjects: "))
+
+percentage = (Marks / 5) * 100
+
+print(Name)
+print(Roll_no)
+print(Marks)
+print(percentage)
+
+
+BILL SPLITER 
+
+Bill_amount = float(input("the total bill is: "))
+no_of_guest = int(input("total no. of guests= "))
+
+formula = Bill_amount / no_of_guest
+
+
+print(Bill_amount)
+print(no_of_guest)
+print(formula)
+
