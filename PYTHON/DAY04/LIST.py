0e4ec6c88c2c50bfa53db4949320b31d88f561b0
aditya-saluja <adityasaluja2007@gmail.com>
2026-07-18 03:23:00 +0530

    Complete Python Day 04 and SQL Day 05

---
+++ PYTHON/DAY04/LIST.py
@@ -0,0 +1,48 @@
+# difference between LIST, TUPLES, SET 
+
+# 1- LIST 
+# ordered hote hein 
+# , add, remove ho skta hein 
+# , duplicates allowed hein []
+
+# 2-TUPLES
+# ordered hein 
+# cannot be changed ek baar bn gye toh 
+# duplicated allowed hein ()
+
+# 3- SET
+# NO FIX ORDER 
+# can be change
+# no duplicates allowed {}
+
+# PLAYERS LIST
+
+# scores = [45, 82, 12, 55, 90, 38, 102]
+
+# for score in scores:
+#     if score > 50:
+#         print(score)
+
+
+# EVEN LIST 
+
+# numbers = [12, 15, 18, 21, 24, 31, 40]
+
+# for number in numbers:
+#     if number % 2 == 0:
+#         print(number)
+
+
+# COUNT POSITIVES
+
+# numbers = [-5, 8, -2, 15, 0, 11]
+
+# count = 0
+
+# for number in numbers:
+#     if number >= 0:
+#         count = count + 1
+
+# print(count)
+        
+
---
+++ PYTHON/DAY04/SHOPPING_CART.py
@@ -0,0 +1,11 @@
+# SHOPPING CART PROBLEM 
+
+
+foods = []
+prices = []
+total = 0 
+
+while True:
+    food = input("enter your item: ")
+    
+
