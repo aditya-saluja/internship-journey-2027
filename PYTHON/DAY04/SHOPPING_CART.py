90a245b64bd4aca161d5c3a16636d1a8e7dc03dd
aditya-saluja <adityasaluja2007@gmail.com>
2026-07-19 18:17:34 +0530

    Add DSA Linear Search practice problems

--- PYTHON/DAY04/SHOPPING_CART.py
+++ PYTHON/DAY04/SHOPPING_CART.py
@@ -1,4 +1,4 @@
-# SHOPPING CART PROBLEM 
+SHOPPING CART PROBLEM 
 
 
 foods = []
@@ -6,6 +6,27 @@ prices = []
 total = 0 
 
 while True:
-    food = input("enter your item: ")
-    
+    food = input("enter your item (q to quit): ")
+    if food.lower() == "q":
+        break
+
+    else:
+        price = float(input("enter a price: "))
+        foods.append(food)
+        prices.append(price)
+
+print("------you are done------")
+
+for food in foods:
+    print(food , end=" ")
+
+for price in prices:
+    total += price
+
+print(total)
+
+
+
+print(total)
+
 
